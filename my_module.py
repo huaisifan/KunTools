b6cd4892cfd0b49d2246da827f1981f4ccf46bd3
huaisifan <39611808+huaisifan@users.noreply.github.com>
2024-07-27 00:24:11 +0800

    Create my_module.py

---
+++ my_module.py
@@ -0,0 +1,16 @@
+
+def my_function(location):
+    """
+    查询地点天气信息
+    """
+    print(f"{location}")
+    return location+"天气信息：晴"
+
+
+
+def my_function1(product_name):
+    """
+    查询商品价格
+    """
+    print(f"{product_name}")
+    return product_name+"价格：100元"
